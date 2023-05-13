45c75feb8deb8edbcf9846ba87389caa2e334577
gilangwokeh <gilangwokeh12@gmail.com>
2023-05-13 16:46:22 +0700

    update

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,59 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
-    const documentList = document.getElementById('documentList');
-    const list = document.getElementById('list');
-  
-    // Fungsi untuk menampilkan daftar dokumen
-    function showDocumentList(documents) {
-      list.innerHTML = '';
-      documents.forEach(doc => {
-        const listItem = document.createElement('li');
-        const title = document.createElement('span');
-        title.textContent = doc.title;
-        listItem.appendChild(title);
-        list.appendChild(listItem);
-      });
-    }
-  
-    // Fungsi untuk mengambil daftar dokumen dari server
-    function getDocuments() {
-      fetch('/documents')
-        .then(response => response.json())
-        .then(data => {
-          showDocumentList(data.documents);
-        })
-        .catch(error => console.log(error));
-    }
-  
-    // Fungsi untuk menampilkan halaman setelah login sukses
-    function showLoggedInPage() {
-      loginForm.classList.add('hidden');
-      documentList.classList.remove('hidden');
-      getDocuments();
-    }
-  
-    // Submit form login
-    loginForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-  
-      fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data.success) {
-            showLoggedInPage();
-          } else {
-            alert('Login gagal');
-          }
-        })
-        .catch(error => console.log(error));
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
+  const documentList = document.getElementById("documentList");
+  const list = document.getElementById("list");
+
+  // Fungsi untuk menampilkan daftar dokumen
+  function showDocumentList(documents) {
+    list.innerHTML = "";
+    documents.forEach((doc) => {
+      const listItem = document.createElement("li");
+      const title = document.createElement("span");
+      title.textContent = doc.title;
+      listItem.appendChild(title);
+      list.appendChild(listItem);
     });
+  }
+
+  // Fungsi untuk mengambil daftar dokumen dari server
+  function getDocuments() {
+    fetch("/documents")
+      .then((response) => response.json())
+      .then((data) => {
+        showDocumentList(data.documents);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  // Fungsi untuk menampilkan halaman setelah login sukses
+  function showLoggedInPage() {
+    loginForm.classList.add("hidden");
+    documentList.classList.remove("hidden");
+    getDocuments();
+  }
+
+  // Submit form login
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("http://localhost:5001/api/route/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          showLoggedInPage();
+        } else {
+          alert("Login gagal");
+        }
+      })
+      .catch((error) => console.log(error));
   });
-  
+});
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const port = 3001;
+const express  = require('express');
+const port     = 5001;
 const hostname = 'localhost';
-const cors =  require('cors');
-const body = require('body-parser');
-const app = express();
+const cors     =  require('cors');
+const body     = require('body-parser');
+const app      = express();
+const mysql    = require('mysql2');
+
 app.use(express.json());
 
 app.use(cors());
@@ -12,6 +14,20 @@ app.use(body.json());
 
 app.use("/api/route", require('./route/routing'))
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'gilang',
+    password: 'password123',
+    database: 'test'
+});
+
+connection.connect((error) => {
+    if (error) {
+      console.error('Error connecting to the database:', error);
+      return;
+    }
+    console.log('Connected to the database!');
+  });
 
 app.listen(port,hostname,()=> {
     console.log(`server menyala di ${hostname}:${port}`);
